e806f0c27420bcfb45cce18c454ab2cb212d2e45
Manuel Cheveste <manuelcheveste@gmail.com>
2023-05-22 15:04:13 -0300

    Store index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :products
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'store#index', as: 'store_index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :products
 end
