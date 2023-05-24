e5c26afda2e26df4a9ae126c8dc2cdec04056d8c
Manuel Cheveste <manuelcheveste@gmail.com>
2023-05-24 14:41:20 -0300

    Offenses

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 class LineItemsController < ApplicationController
   include CurrentCart
   before_action :set_cart, only: [:create]
 
-  before_action :set_line_item, only: %i[ show edit update destroy ]
+  before_action :set_line_item, only: %i[show edit update destroy]
 
   # GET /line_items or /line_items.json
   def index
@@ -10,8 +12,7 @@ class LineItemsController < ApplicationController
   end
 
   # GET /line_items/1 or /line_items/1.json
-  def show
-  end
+  def show; end
 
   # GET /line_items/new
   def new
@@ -19,8 +20,7 @@ class LineItemsController < ApplicationController
   end
 
   # GET /line_items/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /line_items or /line_items.json
   def create
@@ -29,7 +29,7 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart, notice: "Line item was successfully created." }
+        format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -42,7 +42,7 @@ class LineItemsController < ApplicationController
   def update
     respond_to do |format|
       if @line_item.update(line_item_params)
-        format.html { redirect_to line_item_url(@line_item), notice: "Line item was successfully updated." }
+        format.html { redirect_to line_item_url(@line_item), notice: 'Line item was successfully updated.' }
         format.json { render :show, status: :ok, location: @line_item }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -56,7 +56,7 @@ class LineItemsController < ApplicationController
     @line_item.destroy
 
     respond_to do |format|
-      format.html { redirect_to line_items_url, notice: "Line item was successfully destroyed." }
+      format.html { redirect_to line_items_url, notice: 'Line item was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
--- app/models/product.rb
+++ app/models/product.rb
@@ -17,9 +17,9 @@ class Product < ApplicationRecord
 
   # ensure that there are no line items referencing this product
   def ensure_not_referenced_by_any_line_item
-    unless line_items.empty?
-      errors.add(:base, 'Line Items present')
-      throw :abort
-    end
+    return if line_items.empty?
+
+    errors.add(:base, 'Line Items present')
+    throw :abort
   end
 end
